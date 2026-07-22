473d070f9c21646604328fd9068e0a8568848dd7
Mahdi Abdallah <mahdiabdallah751@gmail.com>
2026-07-22 18:58:28 +0300

    Update phone number in site configuration

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   brandName: 'Soft POS',
   domain: 'https://softpos.ca',
-  phone: '1-329-3425',
+  phone: '1-819-329-3425',
   email: 'info@softpos.ca',
   address: '713 Alesther St, Ottawa ON K1K 3B4',
   hours: 'Office: 9:00 AM - 05:00 PM',
